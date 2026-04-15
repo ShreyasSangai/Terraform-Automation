39c9471d91947d7cdc0e911e01331750ff0e41fe
SholayThakur <86546742+ShreyasSangai@users.noreply.github.com>
2026-04-16 02:20:52 +0530

    Update default values for EC2 variables

--- variables.tf
+++ variables.tf
@@ -5,12 +5,12 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "jenkins-ap-south-1" 
+    default     =  "Batch-31" 
 }
 
 variable "instance_type" { 
     description = "instance type for ec2" 
-    default     =  "t2.micro" 
+    default     =  "t3.micro" 
 }
 
 variable "security_group" { 
@@ -24,7 +24,7 @@ variable "tag_name" {
 } 
 variable "ami_id" { 
     description = "AMI for Ubuntu Ec2 instance" 
-    default     = "ami-07ffb2f4d65357b42" 
+    default     = "ami-0e12ffc2dd465f6e4" 
 }
 variable "versioning" {
     type        = bool
